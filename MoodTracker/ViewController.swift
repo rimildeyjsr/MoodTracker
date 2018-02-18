334631db3a953ee90430d0740d82015a685ce1ea
Rimil Dey <rimildeyjsr@gmail.com>
2018-02-18 11:45:43 +0530

    Initial Commit

---
+++ MoodTracker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MoodTracker
+//
+//  Created by Rimil Dey on 18/02/18.
+//  Copyright © 2018 Rimil Dey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
