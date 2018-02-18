185e5e924c6ceaee451cbd5768163c93778e8311
Rimil Dey <rimildeyjsr@gmail.com>
2018-02-18 12:17:50 +0530

    added files for table view controller and table view cell

---
+++ MoodTracker/MoodTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MoodTableViewCell.swift
+//  MoodTracker
+//
+//  Created by Rimil Dey on 18/02/18.
+//  Copyright © 2018 Rimil Dey. All rights reserved.
+//
+
+import UIKit
+
+class MoodTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
