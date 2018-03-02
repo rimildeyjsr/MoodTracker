03ade92bcedd4298d6cf2fe6ed70b4dd7f1afae4
Rimil Dey <rimildeyjsr@gmail.com>
2018-03-03 00:18:33 +0530

    ui done, added 2 files and assigned them

---
+++ MoodTracker/YourEntryTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  YourEntryTableViewCell.swift
+//  MoodTracker
+//
+//  Created by Rimil Dey on 03/03/18.
+//  Copyright © 2018 Rimil Dey. All rights reserved.
+//
+
+import UIKit
+
+class YourEntryTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
